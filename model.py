49c91a1df59daaf34ad8146feb6af4003334e6cc
JiaZe <920687534@qq.com>
2022-02-28 11:41:07 +0800

    Create model.py
    Load model file and define metric function

---
+++ model.py
@@ -0,0 +1,26 @@
+from keras.models import load_model
+from keras import backend as K
+
+smooth = 1.
+
+
+# Metric function
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def jacard(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    union = K.sum(y_true_f + y_pred_f - y_true_f * y_pred_f)
+    return intersection / union
+
+
+def load_model_func(model_path):
+    model = load_model(model_path, compile=False)
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[dice_coef, jacard])
+    return model
